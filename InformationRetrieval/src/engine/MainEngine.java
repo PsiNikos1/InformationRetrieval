8b95998405c0d1a292d070247d682ec1c86f065f
Nikolaos Paralias Psilodimitrakopoulos <>
2025-05-05 23:25:26 +0300

    v 1.0

---
+++ InformationRetrieval/src/engine/MainEngine.java
@@ -0,0 +1,12 @@
+package engine;
+
+import view.SimpleSearchGUI;
+
+public class MainEngine {
+
+	public static void main(String[] args) {
+		SimpleSearchGUI gui = new SimpleSearchGUI();	
+
+	}
+
+}
